b0060a2265eaee31187c808869d7e3c1ad82480e
Valentin Urzua <urzuavales@gmail.com>
2025-01-17 17:17:21 -0600

    Complete creation of Landing Engine; visual details are still pending.

--- app/Opportunities/page.js
+++ app/Opportunities/page.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import WantToStayUpdated from "./Components/WantToStayUpdated"
 
 const pageEvents = () => {
   return (
-    <div className='py-[10rem] px-[7rem]'>
+   <> <div className='py-[10rem] px-[7rem]'>
       <h2 className='text-[10rem] text-ColorPrincipal font-PerformanceMark w-3/5 leading-[8rem]'>Events &
       Opportunities</h2>
     </div>
+    <WantToStayUpdated />
+   </>
   )
 }
 
