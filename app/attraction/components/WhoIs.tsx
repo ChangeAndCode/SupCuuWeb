51d9bb8b465f3cc12acc8fe0f343948bdcc8b5b0
Valentin Urzua <urzuavales@gmail.com>
2025-01-24 10:16:25 -0600

    Video update and integration of startup icon, integration of new image

--- app/attraction/components/WhoIs.tsx
+++ app/attraction/components/WhoIs.tsx
@@ -1,12 +1,12 @@
 const WhoIs = () => {
   return (
     <div className="flex flex-col items-center justify-center pt-[6rem] md:pt-[9rem] xl:pt-[13rem] pb-[9rem] sm:pb-[11rem] md:pb-[17rem] px-[2rem] sm:px-[4rem] md:px-[4rem] lg:px-[6rem] 2xl:px-0">
-      <h2 className='font-pragmatica main-Tipography text-ColorPrincipal uppercase text-[1.5rem] sm:text-[2rem] md:text-[3rem]'>what is chihuahua?</h2>
+      <h2 className='font-pragmatica main-Tipography text-ColorPrincipal uppercase text-[1.5rem] sm:text-[2rem] md:text-[3rem]'>who is chihuahua?</h2>
       <div className="mt-[3rem] w-full max-w-[1200px] border-[.4rem] border-ColorPrincipal">
         <div className="relative w-full" style={{ paddingBottom: '56.25%' }}>
           <iframe
             className="absolute top-0 left-0 w-full h-full"
-            src="https://www.youtube.com/embed/xyj8pYNECSQ?si=tPt8S1pjaGDoPvt7"
+            src="https://www.youtube.com/embed/jVe-7hgynDI?si=az7wvDwGxg7GftLy"
             title="YouTube video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
