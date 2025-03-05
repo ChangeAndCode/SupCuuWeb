6acb45b1294e4bdb9fd6c9cde9b2268c55d82b1d
Valentin Urzua <urzuavales@gmail.com>
2025-03-05 15:59:17 -0600

    Number changed, and links were added to direct to different social networks.

--- app/form/components/RedesSociales.tsx
+++ app/form/components/RedesSociales.tsx
@@ -9,6 +9,24 @@ const RedesSociales: React.FC = () => {
     setMounted(true);
   }, []);
 
+  const redesSociales = [
+    { 
+      src: '/redesSociales/insta.webp', 
+      alt: 'Instagram',
+      href: 'https://www.instagram.com/startupchihuahua/'
+    },
+    { 
+      src: '/redesSociales/link.webp', 
+      alt: 'LinkedIn',
+      href: 'https://www.linkedin.com/company/startupchihuahua/ '
+    },
+    { 
+      src: '/redesSociales/facebook.webp', 
+      alt: 'Facebook',
+      href: 'https://www.facebook.com/startupchih'
+    }
+  ];
+
   if (!mounted) {
     return <div className='h-[200px] animate-pulse bg-gray-200/20 rounded-lg' />;
   }
@@ -19,20 +37,23 @@ const RedesSociales: React.FC = () => {
         @startupchihuahua
       </p>
       <div className='flex lg:justify-end'>
-        {[
-          { src: '/redesSociales/insta.webp', alt: 'Instagram' },
-          { src: '/redesSociales/link.webp', alt: 'LinkedIn' },
-          { src: '/redesSociales/facebook.webp', alt: 'Facebook' }
-        ].map((img) => (
-          <Image
-            key={img.alt}
-            src={img.src}
-            alt={img.alt}
-            width={75}
-            height={75}
-            quality={80}
-            priority
-          />
+        {redesSociales.map((red) => (
+          <a
+            key={red.alt}
+            href={red.href}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:opacity-80 transition-opacity"
+          >
+            <Image
+              src={red.src}
+              alt={red.alt}
+              width={75}
+              height={75}
+              quality={80}
+              priority
+            />
+          </a>
         ))}
       </div>
     </div>
